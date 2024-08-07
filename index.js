6c093c75db4c66ea2e86d6e4e4b39c2b59ba6557
Oltiariq3561 <oltiariq356@gmail.com>
2024-08-06 23:28:24 -0700

    tugadi

---
+++ index.js
@@ -0,0 +1,233 @@
+// let arr = [1,2,3,4,5];
+// function test(arg) {
+//     let res = 0
+//     for (const i of arg) {
+//         if(i%2==1){
+//         res ++;
+//         }
+//     }
+//     return res
+// }
+// let res = test(arr);
+// console.log(res);
+
+// 2-masala
+// let arr = 'salom111';
+// function test(arg) {
+//     let res = [];
+//     for (const i of arg) {
+//         if (i>0) {
+//             res.push(i)
+//         }
+//     }
+//     return res
+// }
+// let res = test(arr);
+// console.log(res);
+
+// 3-masala
+// let arr = 'onpic iopemvd oincfirw'
+// function test(arg) {
+//     let res = arg.split(' ')
+//     let counter = 0
+//     for (const i of res) {
+//         counter ++;
+//     }
+//     return counter;
+// }
+// let res = test(arr);
+// console.log(res);
+
+// 4-masala
+// let arr = [1,2,3,4,5];
+// function test(arg) {
+//     let min = arg[0];
+//     for (const i of arg) {
+//         if (i<min) {
+//             min = i
+//         }
+//     }
+//     return min;
+// }
+// let res = test(arr);
+// console.log(res);
+
+// 5-masala
+// let arr = 'salom'
+// function test(arg) {
+//     let res = arg.split('').reverse().join('');
+//     return res
+// }
+// let res = test(arr);
+// console.log(res);
+
+// 6-masala
+// let a = 5;
+// function test(arg) {
+//     let res = 1;
+//     for (let i = 1; i <= arg; i++) {
+//         res*=i
+//     }
+//     return res
+// }
+// let res = test(a);
+// console.log(res);
+
+// 7-masala
+// let arr =[2,4,1,6,3,7]
+// function test(arg) {
+//     return arg.sort(function(a,b) {
+//         return a - b;
+//     })
+// }
+// let res = test(arr);
+// console.log(res);
+
+// 8-masala
+// let arr = "salom aleykum";
+// function test(s){
+//     let res = s.replace('salom'," ")
+//     return res
+// }
+// let res = test(arr);
+// console.log(res);
+
+// 9-masala
+// let arr = ['salom','alik']
+// function test(arg) {
+//     let res = []
+//     for (const i of arg) {
+//         res.push(i.toUpperCase())
+//     }
+//     return res;
+// }
+// let res = test(arr);
+// console.log(res);
+
+// 10-masala
+// let arr = [1,2,3,4,5,6,7];
+// function test(arg) {
+//     let res = [];
+//     for (const i of arg) {
+//         if (i%2==1) {
+//             res.push(i)
+//         }
+//     }
+//     return res
+// }
+// let res = test(arr);
+// console.log(res);
+
+// 11-masala
+// let arr = ['salom','alikkkk','qodirhon'];
+// function test(arg) {
+//     let max = arg[0];
+//     let res = ''
+//     for (const i of arg) {
+//         if (i.length > max.length) {
+//             res = i
+//         }
+//     }
+//     return res
+// }
+// let res = test(arr);
+// console.log(res);
+
+// 12-masala
+// let arr = [1,2,3,4,5,6,7];
+// function test(arg) {
+//     let res = 0;
+//     for (const i of arg) {
+//      res+=i
+//     }
+//     return res
+// }
+// let res = test(arr);
+// console.log(res);
+
+// 13-masala
+// let arr = [1,2,-3,4,5,-6,7];
+// function test(arg) {
+//     let res = [];
+//     for (const i of arg) {
+//         if (i>0) {
+//             res.push(i)
+//         }
+//     }
+//     return res
+// }
+// let res = test(arr);
+// console.log(res);
+
+// 14-masala
+// let arr = ['salom','alikkkk','qodirhon'];
+// function test(arg) {
+//     let res = [];
+//     for (const i of arg) {
+//         res.push(i.length)
+//     }
+//     return res
+// }
+// let res = test(arr);
+// console.log(res);
+
+// 15-masala
+// let arr = [1,2,3,4,5,6,7];
+// function test(arg) {
+//     let res = [];
+//     for (const i of arg) {
+//          res.push(i**2)
+//     }
+//     return res
+// }
+// let res = test(arr);
+// console.log(res);
+
+// 17-masala
+// let arr = [1,2,3,4,5,6,7];
+// function test(arg) {
+//     let res = arg.reverse();
+//     return res
+// }
+// let res = test(arr);
+// console.log(res);
+
+// 18masala
+// let arr = ['salom','alik'];
+// function test(arg) {
+//     return arg.sort();
+// }
+// let res = test(arr);
+// console.log(res);
+
+// 19-masala
+// let arr = 'salom alik';
+// function test(arg) {
+//    let res = arg.split(' ');
+//    return res.sort().join(' ');
+
+// }
+// let res = test(arr);
+// console.log(res);
+
+// 21-masala
+// let arr = 'salom alik';
+// function test(arg) {
+// let res = arg.split(' ').join('')
+// return res
+// }
+// let res = test(arr);
+// console.log(res);
+
+// 23-masala
+// let arr = 'salom alik';
+// function test(arg) {
+//     let res = arg.split(' ')
+//     let sum = 0
+//     for (const i of res) {
+//         sum+=i.length
+//     }
+//     return sum
+//     }
+//     let res = test(arr);
+//     console.log(res);
